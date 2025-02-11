579174334228976963e523d94d97f3143964c8c5
Karthik G K <kar221694@gmail.com>
2025-02-11 21:43:41 +0530

    Update file_handler.c

--- file_handler.c
+++ file_handler.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 
+int main(){
+// fd is called file descriptor that is a smalletst non negative number the linux will provide when we run open system call.
+int fd = open("newfile.txt", O_RDONLY | O_WRONLY | O_RDWR);
+if(fd == -1){
+  printf("failed to open file error : %d\n", errno);
+  perror("Error :");
+}
+else {
+        printf("SUCCESSUFLLY FILE CREATED\n");
+}
+
+return 0;
+}
