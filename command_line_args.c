71ccfbc1219a9a05aee1644250038c13192252f0
Karthik G K <kar221694@gmail.com>
2025-02-16 14:40:39 +0530

    Update command_line_args.c

--- command_line_args.c
+++ command_line_args.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
+extern char** environ;
 int main(int argc, char* argv[]){
     // print all command line args that you will receive
     for(int i=0; i<=argc; i++){
         printf("Argument %d : %s\n", i, argv[i]);
     }
+
+   char** env;
+    for(env=environ; *env!=NULL; env++){
+        printf("%s\n", *env);
+    }
+
+    putenv("SHELL=/bin/bash");
+    printf("KARTHIK G K : %s\n", getenv("SHELL"));
     return 0;
 }
