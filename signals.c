e370b6a3654d71755637dec113d102aaf0b3adfa
Karthik G K <kar221694@gmail.com>
2025-02-18 22:01:38 +0530

    Create signals.c

---
+++ signals.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    // signals are the software interrupts that are sent to a process to notify that a particular event has occurred.
+    // basically an asyncronous notification to a process.
+    return 0;
+}
