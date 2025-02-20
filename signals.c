140e47c10b1e569a0935d64e49f2fe0cbccc49d0
Karthik G K <kar221694@gmail.com>
2025-02-20 21:58:53 +0530

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <signal.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+void handle_signal(int signal){
+    printf("Signal caught\n %d", signal);
+    exit(-1);
+}
+
+void handle_alaram(int sig){
+    printf("Alarm signal caught\n");
+    // overriden handler here
+    signal(SIGALRM, SIG_IGN);
+    alarm(2);
+    exit(EXIT_SUCCESS);
+}
 
 int main(){
     // signals are the software interrupts that are sent to a process to notify that a particular event has occurred.
     // basically an asyncronous notification to a process.
+
+    // signal() function is used to handle signals.
+    printf("%d : is my processId\n", getpid());
+    if(signal(SIGINT, handle_signal) == SIG_ERR){
+        printf("Error in handling sigint signal\n");
+    }
+    if(signal(9999, handle_signal) == SIG_ERR){
+        printf("Error in handling signal\n");
+    }
+    
+    if(signal(SIGTERM, handle_signal) == SIG_ERR){
+        printf("Error in handling sigterm signal\n");
+    }
+
+    if(signal(SIGALRM, handle_alaram) == SIG_ERR){
+        printf("Error in handling sigalarm signal\n");
+    }
+
+    alarm(2);
+
+    while(1);
+
     return 0;
 }
