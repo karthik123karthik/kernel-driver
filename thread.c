117e07780a7046b42e20d279ef60a6fc5f182c54
Karthik G K <kar221694@gmail.com>
2025-09-02 21:30:30 +0530

    Update thread.c

--- thread.c
+++ thread.c
@@ -30,3 +30,31 @@ int main(){
     pthread_exit(0); //if I use this then main thread will exit and all other threads will continue to run
     // return 0;
 }
+
+
+// Pthread DETACH
+
+// threads are by default joinable threads
+// so if it is not joined it will lead to zombie thread
+// so avoid this we can use pthread_detach
+
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+static void* thread_function(void* args){
+    pthread_detach(pthread_self());
+    printf("I am second thread exiting without creating zombie process\n");
+}
+
+int main(){
+    pthread_t t;
+
+    if(pthread_create(&t, NULL, thread_function, NULL) != 0){
+        perror("Failed to create thread");
+        return 1;
+    }
+
+    return 0;
+}
